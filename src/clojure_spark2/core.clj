7c92ce8d7df68ec20026c44a7191adab54627e0d
Narendra Pinnaka <npinnaka@yahoo.com>
2018-11-04 06:01:38 -0600

    updated code to complete flatterning..

--- src/clojure_spark2/core.clj
+++ src/clojure_spark2/core.clj
@@ -16,6 +16,14 @@
 
 (def spark (build-spark-session "app"))
 
+(def departments ["IT" "FINANCE" "SALES" "MARKETING" "OPERATIONS" "HR"])
+
+(defn prepare-coverage-fields[key-prefix v]
+  (merge {} {(str key-prefix "_department") v}
+         {(str key-prefix "_emp_count") v}
+         {(str key-prefix "_expenses") v}))
+
+
 (defn create-map
   "create a map using zipmap"
   [columns values]
@@ -65,12 +73,68 @@
   (if (contains? in-map :department)
     (into {} (keep #(x % (:department in-map)) in-map))))
 
-(map transform-map final-map)
+(def out-map (map transform-map final-map))
 
+(defn abc[item]
+  (into (sorted-map) (filter #(= item (get % "id")) out-map)))
 
-;
+(defn xys[unique-keys out-map]
+  (map abc unique-keys))
+
+(defn xc [in-map]
+  (merge (into {} (map #(prepare-coverage-fields % nil) departments)) in-map)
+  )
+
+(defn final-output-vector[in-vec-map]
+  (map xc in-vec-map)
+  )
+
+(->
+ (distinct (map #(get % "id") out-map))
+ (xys out-map)
+ (final-output-vector))
+
 ;(defn read-json[sparkSession file-name]
 ;  (println (type sparkSession))
 ;  (sql/json-file sparkSession file-name))
 ;
 ;(.show (read-json((build-spark-session "abc"), "resources/cartypes.json")))
+
+;({"FINANCE_department"    nil,
+;  "SALES_emp_count"       20,
+;  "MARKETING_expenses"    33000,
+;  "HR_emp_count"          nil,
+;  "MARKETING_emp_count"   35,
+;  "SALES_department"      "SALES",
+;  "FINANCE_expenses"      nil,
+;  "IT_expenses"           45000,
+;  "OPERATIONS_expenses"   nil,
+;  "id"                    1,
+;  "OPERATIONS_department" nil,
+;  "FINANCE_emp_count"     nil,
+;  "OPERATIONS_emp_count"  nil,
+;  "MARKETING_department"  "MARKETING",
+;  "HR_department"         nil,
+;  "IT_emp_count"          250,
+;  "SALES_expenses"        32000,
+;  "HR_expenses"           nil,
+;  "IT_department"         "IT"}
+;  {"FINANCE_department"    "FINANCE",
+;   "SALES_emp_count"       nil,
+;   "MARKETING_expenses"    nil,
+;   "HR_emp_count"          32,
+;   "MARKETING_emp_count"   nil,
+;   "SALES_department"      nil,
+;   "FINANCE_expenses"      19000,
+;   "IT_expenses"           nil,
+;   "OPERATIONS_expenses"   24000,
+;   "id"                    2,
+;   "OPERATIONS_department" "OPERATIONS",
+;   "FINANCE_emp_count"     5,
+;   "OPERATIONS_emp_count"  300,
+;   "MARKETING_department"  nil,
+;   "HR_department"         "HR",
+;   "IT_emp_count"          nil,
+;   "SALES_expenses"        nil,
+;   "HR_expenses"           17000,
+;   "IT_department"         nil})
